4f4cb3aef3d0f4dd2ffb26db42fb789f00fa2f4f
jianwangzheng <275903851@qq.com>
2020-05-04 15:28:50 +0800

    第二次提交15:28:43

--- src/com/dlc/test/GitTest1.java
+++ src/com/dlc/test/GitTest1.java
@@ -4,4 +4,7 @@ public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("hello,github");
     }
+    public void sayHello(){
+        System.out.println("say hello 15:28");
+    }
 }
